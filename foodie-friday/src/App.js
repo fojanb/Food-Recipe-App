f19568edd958c911d7f10a86ea9c9e4c847f56cc
Fojan Babaali <fozhanmail@gmail.com>
2021-03-03 23:42:46 -0800

    Update App.js

--- foodie-friday/src/App.js
+++ foodie-friday/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+// import InputComponent from "./components/InputComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    // Authenticate API --> Use Environment Tools to protect the APP_ID and APP_KEY
+    APP_ID: "0a558300",
+    APP_KEY: "181f226856b2e1b79a5770bcf412a99a",
+    exampleReq: `https://api.edamam.com/search?q=chicken&app_id={this.state.APP_ID}&app_key={this.state.APP_KEY}`,
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Foodie Friday</h1>
+        <form className="search-form">
+          <input type="text" className="search-bar"></input>
+          <button type="submit" className="search-button">Search</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
