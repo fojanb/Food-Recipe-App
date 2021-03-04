e710e053d8431494807406b694fd004ffdbcc7e4
Fojan Babaali <fozhanmail@gmail.com>
2021-03-03 23:42:43 -0800

    Create InputComponent.js

---
+++ foodie-friday/src/components/InputComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class InputComponent extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                
+            </div>
+         );
+    }
+}
+ 
+export default InputComponent;
