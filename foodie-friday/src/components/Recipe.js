b20062b9185601cffbf58e9cb10ccaef06eb284d
Fojan Babaali <fozhanmail@gmail.com>
2021-03-05 00:36:41 -0800

    Update Recipe.js

--- foodie-friday/src/components/Recipe.js
+++ foodie-friday/src/components/Recipe.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const Recipe = () => {
-    return
-
-}
+const Recipe = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <p>{props.calories}</p>
+      <img src={props.image} alt={props.title} />
+    </div>
+  );
+};
+export default Recipe;
