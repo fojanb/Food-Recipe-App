e01943a5e80d0534ff6546d28734b1f58877b4b9
Fojan Babaali <fozhanmail@gmail.com>
2021-03-05 12:23:21 -0800

    Update App.js

--- foodie-friday/src/App.js
+++ foodie-friday/src/App.js
@@ -7,34 +7,52 @@ const App = () => {
   // Authenticate API --> Use Environment Tools to protect the APP_ID and APP_KEY :)
   const APP_ID = "0a558300";
   const APP_KEY = "181f226856b2e1b79a5770bcf412a99a";
-
+  // ***********************State Updating**********************
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+  // ***********************************************************
   //We need useEffect() while working with API in react function component
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
+  // ***********************************************************
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
     // console.log(data.hits); //hits has all the recipes
-    setRecipes(data.hits);
+    setRecipes(data.hits); // data go to reciepes
   };
-  const updateSearch = (event) => {
-    setSearch(event.target.value);
-    console.log(search);
-  };
-  // __________________________________________________________________________
+  // ***********************EventHandler************************
 
+  const updateSearch = (event) => {
+    setSearch(event.target.value); //"search" will get this e.target.value
+    // console.log(search);
+  };
+  const getSearch = (event) => {
+    event.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+  const styles = {
+    display: "flex",
+    justifyContent:"center",
+    // color:"white"
+  };
+  const spanStyle = {
+    fontSize:"20px"
+  }
+  // ***********************************************************
   return (
     <div className="App">
-      <h1>Foodie Friday</h1>
-      <form className="search-form">
+      <h1 style={styles}><span style={spanStyle}>🥑&nbsp;🍓&nbsp;🍬&nbsp;🍹&nbsp;🍪&nbsp;</span>Foodie Friday<span style={spanStyle}>&nbsp;🍰&nbsp;🍩&nbsp;🍄&nbsp;🍦&nbsp;🍉</span></h1>
+      <form className="search-form" onSubmit={getSearch}>
         <input
           type="text"
+          placeholder="🍽 Type Something Yummy ! "
           className="search-bar"
           value={search}
           onChange={updateSearch}
@@ -44,15 +62,18 @@ const App = () => {
         </button>
       </form>
       {/* List of Recipe components : */}
-      {recipes.map((recipe, index) => (
-        <Recipe
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image}
-          key={recipe.recipe.label}
-          // key should be something unique
-        />
-      ))}
+      <div className="recipes">
+        {recipes.map((recipe, index) => (
+          <Recipe
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+            key={recipe.recipe.label}
+            // key should be something unique that's why I used "label"
+          />
+        ))}
+      </div>
     </div>
   );
 };
